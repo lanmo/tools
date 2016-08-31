c241969efb817e66382ebecaed584fae95574d4e
yangnan3 <yannan3@le.com>
2016-08-31 14:30:01 +0800

    pom中添加jar包依赖

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,52 +1,18 @@
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by yangnan on 16/8/22.
  */
 public class Test {
 
-    public static void main(String[] args) throws InterruptedException {
-
-        Test t = new Test();
-
-        B b = t.new B();
-
-        System.out.println(JSONObject.toJSONString(b));
-       Method[] ms = b.getClass().getMethods();
-        for (Method m : ms) {
-            System.out.println(m.getName());
-        }
-
-    }
-
-    class A {
-        private int i;
-
-        public int getI() {
-            return i;
-        }
-
-        public void setI(int i) {
-            this.i = i;
-        }
-    }
-
-    class B extends A {
-        private int b;
-
-        public int getB() {
-            return b;
-        }
-
-        public void setB(int b) {
-            this.b = b;
-        }
+    public static void main(String[] args)  {
+        int a = 0;
     }
 
 }
