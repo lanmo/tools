d85a240ce76417f038aa0141a6a56ebcede600c4
yangnan3 <yannan3@le.com>
2016-12-23 18:04:36 +0800

    gua获取路径的类

--- src/test/java/com/yn/tools/test/JsonTest.java
+++ src/test/java/com/yn/tools/test/JsonTest.java
@@ -1,15 +1,16 @@
 package com.yn.tools.test;
 
 import com.alibaba.fastjson.JSON;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yn.tools.monitor.Monitor;
 import com.yn.tools.netty.msg.User;
-import com.yn.tools.utils.L;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by yangnan on 16/12/13.
@@ -27,13 +28,89 @@ public class JsonTest {
 
         }
 
-        ObjectMapper mapper = new ObjectMapper();
-        for (int i=0;i <100000;i++) {
-            JSON.toJSONString(new User());
-            mapper.writeValueAsString(new User());
+//        oneThread(users, num);
+//        muThread(users, num);
 
+        testParse(num);
+    }
+
+    public static void muThread(List<User> users, int num) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ExecutorService executorService = Executors.newFixedThreadPool(1000);
+        ExecutorService executorService2 = Executors.newFixedThreadPool(1000);
+        CountDownLatch countDownLatch = new CountDownLatch(num);
+        CountDownLatch countDownLatch2 = new CountDownLatch(num);
+        Monitor.begin();
+        for (User u : users) {
+            executorService.execute(new FastJsonThread(countDownLatch, u, null));
         }
 
+        try {
+            countDownLatch.await();
+            Monitor.end("fastjson encode");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Monitor.begin();
+        for (User u : users) {
+            executorService2.execute(new JacksonThread(countDownLatch2, u, mapper, null));
+        }
+
+        try {
+            countDownLatch2.await();
+            Monitor.end("jackson encode");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        executorService.shutdown();
+        executorService2.shutdown();
+
+    }
+
+    public static void testParse(int num) {
+        String str = "{\"userId\":379356,\"userName\":\"I 379356\"}";
+        List<String> strs = new ArrayList<String>(num);
+        for (int i=0; i<num; i++) {
+            strs.add(new String(str));
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        ExecutorService executorService = Executors.newFixedThreadPool(1000);
+        ExecutorService executorService2 = Executors.newFixedThreadPool(1000);
+        CountDownLatch countDownLatch = new CountDownLatch(num);
+        CountDownLatch countDownLatch2 = new CountDownLatch(num);
+        Monitor.begin();
+        for (String s : strs) {
+            executorService.execute(new FastJsonThread(countDownLatch, null, s));
+        }
+
+        try {
+            countDownLatch.await();
+            Monitor.end("fastjson parse");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Monitor.begin();
+        for (String s : strs) {
+            executorService2.execute(new JacksonThread(countDownLatch2, null, mapper, s));
+        }
+
+        try {
+            countDownLatch2.await();
+            Monitor.end("jackson parse");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        executorService.shutdown();
+        executorService2.shutdown();
+    }
+
+    public static void oneThread(List<User> users, int num) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
         Monitor.begin();
         for (User u : users) {
             String str = mapper.writeValueAsString(u);
@@ -54,7 +131,7 @@ public class JsonTest {
 
         Monitor.begin();
         for (String s : strs) {
-           User u = mapper.readValue(s, User.class);
+            User u = mapper.readValue(s, User.class);
         }
         Monitor.end("jackson parse");
 
@@ -63,5 +140,53 @@ public class JsonTest {
             User u = JSON.parseObject(s, User.class);
         }
         Monitor.end("fastjson parse");
+    }
+
+    static class FastJsonThread implements Runnable {
+
+        private CountDownLatch countDownLatch;
+        private User u;
+        private String str;
+        private FastJsonThread(CountDownLatch countDownLatch, User u, String str) {
+            this.countDownLatch = countDownLatch;
+            this.u = u;
+            this.str = str;
+        }
+
+        public void run() {
+            try {
+//                JSON.toJSONString(u);
+                JSON.parseObject(str, User.class);
+            } catch (Exception e) {
+
+            } finally {
+                countDownLatch.countDown();
+            }
+        }
+    }
+
+    static class JacksonThread implements Runnable {
+
+        private CountDownLatch countDownLatch;
+        private User u;
+        private ObjectMapper mapper;
+        private String str;
+        private JacksonThread(CountDownLatch countDownLatch, User u, ObjectMapper mapper, String str) {
+            this.countDownLatch = countDownLatch;
+            this.mapper = mapper;
+            this.u = u;
+            this.str = str;
+        }
+
+        public void run() {
+            try {
+//                this.mapper.writeValueAsString(u);
+                mapper.readValue(str, User.class);
+            } catch (Exception e) {
+
+            } finally {
+                countDownLatch.countDown();
+            }
+        }
     }
 }
